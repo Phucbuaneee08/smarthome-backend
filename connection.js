47985e87e7649b10fe37b834d587d846f29d8e9e
dungnq4256 <dunghlhh@gmail.com>
2022-12-28 16:12:00 +0700

    update mongodb_url

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connection= async(req,res)=>{
     try{
-        const conn = await mongoose.connect(`mongodb+srv://vercel-admin-user:jVSyDuBZbRW7CSph@smarthome.vexghpd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+        const conn = await mongoose.connect(`mongodb+srv://vercel-admin-user:jVSyDuBZbRW7CSph@smarthome.vexghpd.mongodb.net/smarthome?retryWrites=true&w=majority`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
