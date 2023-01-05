7d930b9a8e7151976a7d85cad47a60795d10e6f0
dungnq4256 <dunghlhh@gmail.com>
2023-01-05 12:38:39 +0700

    update

--- routers/deviceRouter.js
+++ routers/deviceRouter.js
@@ -3,11 +3,11 @@ const express = require('express')
 const deviceController = require('../controllers/deviceController')
 const router = express.Router()
 
-router.post('/control',control)
-router.delete('/',deleteDevice)
-router.post('/',createDevice)
-router.get('/:deviceId',getDevice)
-router.get('/:type/:roomId',getData)
+// router.post('/control',control)
+// router.delete('/',deleteDevice)
+// router.post('/',createDevice)
+// router.get('/:deviceId',getDevice)
+// router.get('/:type/:roomId',getData)
 
 //create device
 router.post('/api/device/create-device', deviceController.createDevice);
@@ -15,10 +15,10 @@ router.post('/api/device/create-device', deviceController.createDevice);
 //delete device
 router.delete('/api/device/delete-device', deviceController.deleteDevice);
 
-//control device
-router.put('/api/device/control', deviceController.controlDevice);
+// //control device
+// router.put('/api/device/control', deviceController.controlDevice);
 
-//get device data
-router.get('/api/device/detail', deviceController.getDeviceData);
+// //get device data
+// router.get('/api/device/detail', deviceController.getDeviceData);
 
 module.exports = router
