bdfe572208d890fae8c2d76274695bf31ed6b61f
dungnq4256 <dunghlhh@gmail.com>
2023-02-05 23:11:01 +0700

    update

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -4,7 +4,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
+    api_key: '323516433647223',
     api_secret: process.env.CLOUDINARY_SECRET
 });
 
